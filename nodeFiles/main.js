fea838fff788bff5b39ac635c4f1572868049dca
Marek <marek.stryjenski@gmail.com>
2021-05-27 22:17:23 +0200

    connecting to database.
    running Crud operations
    
    getting current iota value
    
    exchanging iotato kwh

--- nodeFiles/main.js
+++ nodeFiles/main.js
@@ -1,5 +1,16 @@
 async function run() {
-    const { ClientBuilder } = require('@iota/client');
+    var iotaValue = 0;
+    await getIotaValue().then(function (elem) {
+        iotaValue = elem;
+    }).catch((err) => setImmediate(() => {
+        throw err;
+    }));
+
+    console.log(iotaValue);
+
+    const connectionDb = require('./connection.js');
+    const {ClientBuilder} = require('@iota/client');
+
 
     // client will connect to testnet by default
     const client = new ClientBuilder()
@@ -7,11 +18,60 @@ async function run() {
         .build()
 
     const outputs = await client.getAddressOutputs('iota1qz4qx5xrl59wnnvswxk4mjvhjkdk25yveft3us2hgxd5tn2l6gz4vnwld2d');
-    console.log(outputs);
+    // console.log(outputs);
 
-    const output = await client.getOutput('3db5bd50759db2cd15515fbeeb955c4c1eeb38111f738d5fe7eed664ec53ccbf0100');
-    console.log(output);
+    for (var i = 0; i < outputs.length; i++) {
+
+        const output = await client.getOutput(outputs[i]);
+        var transactionId = output.transactionId;
+        var iotaAmount = output.amount;
+        var kwhConv=(iotaAmount/10000)/13.19;
+        console.log(kwhConv);
+        // console.log(output);
+
+        // console.log(transactionId);
+        connectionDb.selectTransactions(transactionId).then(function (rows) {
+            if (rows.length == 0) {
+                connectionDb.insertTransaction(transactionId,iotaValue,iotaAmount,kwhConv);
+            }
+            console.log(rows);
+        }).catch((err) => setImmediate(() => {
+            throw err;
+        }));
+    }
+
     // https://chrysalis-nodes.iota.org
 }
 
-run()
+async function getIotaValue() {
+    const CoinGecko = require('coingecko-api');
+
+//2. Initiate the CoinGecko API Client
+    const CoinGeckoClient = new CoinGecko();
+
+//3. Make calls
+    return new Promise(function (resolve) {
+        async function cos() {
+            let dataObt = await CoinGeckoClient.simple.price({
+                ids: ['iota'],
+                vs_currencies: ['usd'],
+            });
+            // console.log(dataObt);
+            var keys = Object.keys(dataObt.data);
+            var subkeys = Object.keys(dataObt.data[keys[0]]);
+            var currentIotaValue = dataObt.data[keys[0]][subkeys[0]];
+            // console.log(currentIotaValue);
+            resolve(currentIotaValue);
+        }
+
+        cos();
+    });
+
+}
+
+run();
+
+// getIotaValue().then(r => console.log(r));
+
+
+
